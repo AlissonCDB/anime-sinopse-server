5df1f81febc39e75cd40c7bdb3dc6aee126e0d9e
AlissonCDB <alissoncunhadebritto@gmail.com>
2023-07-05 14:54:05 -0300

    Agora vai?

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({origin: "*", methods: "GET,POST"}))
 app.use('/animes', rotaAnime)
 app.use('/listaLeitura', rotaLivro)
 
-const port = '8000'
+const port = '80'
 
 app.listen(port, () => {
     console.log(`Escutando a porta ${port}`)
