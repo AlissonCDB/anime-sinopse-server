5f96bb5334c918e7e191bf4610e182701f5bd004
AlissonCDB <alissoncunhadebritto@gmail.com>
2023-07-04 21:17:18 -0300

    Update listaLeitura 2

--- rotas/listaLeitura.js
+++ rotas/listaLeitura.js
@@ -2,6 +2,11 @@ const { Router } = require("express")
 const { getLivros, postLivro } = require("../controladores/listaLeitura")
 const router = Router()
 
+router.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    next();
+  });
+
 router.get('/', getLivros)
 
 router.post('/', postLivro)
