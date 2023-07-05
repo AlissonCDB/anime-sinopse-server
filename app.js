23af11205448abc22079e5280917f09c25f13496
AlissonCDB <alissoncunhadebritto@gmail.com>
2023-07-04 21:33:46 -0300

    Teste 01

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-const express = require("express");
-const rotaAnime = require('./rotas/animes');
-const cors = require("cors");
-const rotaLivro = require("./rotas/listaLeitura");
+const express = require("express")
+const rotaAnime = require('./rotas/animes')
+const cors = require("cors")
+const rotaLivro = require("./rotas/listaLeitura")
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
+app.use(cors({origin: "http://localhost:3000", methods: ["PUT", "PATCH", "DELETE"] }))
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use('/animes', rotaAnime)
+app.use('/listaLeitura', rotaLivro)
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  next();
-});
-
-app.use('/animes', rotaAnime);
-app.use('/listaLeitura', rotaLivro);
-
-const port = '8000';
+const port = '8000'
 
 app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`);
-});
+    console.log(`Escutando a porta ${port}`)
+})
--- rotas/listaLeitura.js
+++ rotas/listaLeitura.js
@@ -2,11 +2,6 @@ const { Router } = require("express")
 const { getLivros, postLivro } = require("../controladores/listaLeitura")
 const router = Router()
 
-router.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    next();
-  });
-
 router.get('/', getLivros)
 
 router.post('/', postLivro)
