483768524e0c5b31466b6de1c0e4860c8a75bde2
AlissonCDB <alissoncunhadebritto@gmail.com>
2023-07-04 22:10:03 -0300

    Teste 10

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const rotaAnime = require('./rotas/animes');
-const rotaLivro = require('./rotas/listaLeitura');
+const express = require("express")
+const rotaAnime = require('./rotas/animes')
+const cors = require("cors")
+const rotaLivro = require("./rotas/listaLeitura")
 
-const app = express();
-const port = 8000;
+const app = express()
+app.use(express.json())
+app.use(cors({origin: "*"}))
 
-app.use(cors({ origin: 'http://localhost:3000', methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'] }));
-app.use(express.json());
+app.use('/animes', rotaAnime)
+app.use('/listaLeitura', rotaLivro)
 
-app.use('/animes', rotaAnime);
-app.use('/listaLeitura', rotaLivro);
+const port = '8000'
 
 app.listen(port, () => {
-  console.log(`Escutando na porta ${port}`);
-});
+    console.log(`Escutando a porta ${port}`)
+})
