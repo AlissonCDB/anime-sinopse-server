ef597712d7b48d7bb8945a62578434de12acd62c
AlissonCDB <alissoncunhadebritto@gmail.com>
2023-07-05 15:43:42 -0300

    Tá quase ou não

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors({origin: "*", methods: "GET,POST"}))
 
 app.use('/animes', rotaAnime)
-app.use('/listaLeitura', rotaLivro)
+app.use('/listaLeitura.json', rotaLivro)
 
 const port = '80'
 
